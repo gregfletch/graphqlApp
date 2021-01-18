daeb66dbca626d0098b9e9ad96f25240b5e6b899
Greg Fletcher <gregfletch@gmail.com>
2021-01-18 15:42:44 -0500

    Lint fixes

--- src/app/guards/user.guard.ts
+++ src/app/guards/user.guard.ts
@@ -9,7 +9,7 @@ import { AuthService } from 'src/app/services/auth.service';
 export class UserGuard implements CanActivate {
   constructor(public authService: AuthService, public router: Router) {}
 
-  canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
+  canActivate(_route: ActivatedRouteSnapshot, _state: RouterStateSnapshot): Observable<boolean | UrlTree> | Promise<boolean | UrlTree> | boolean | UrlTree {
     if (this.authService.isAuthenticated() && !this.authService.isTokenExpired()) {
       this.router.navigate(['/']);
       return false;
--- src/app/register/register-routing.module.ts
+++ src/app/register/register-routing.module.ts
@@ -3,9 +3,7 @@ import { RouterModule, Routes } from '@angular/router';
 import { UserGuard } from 'src/app/guards/user.guard';
 import { RegisterComponent } from 'src/app/register/register.component';
 
-const routes: Routes = [
-  { path: '', component: RegisterComponent, canActivate: [UserGuard] }
-];
+const routes: Routes = [{ path: '', component: RegisterComponent, canActivate: [UserGuard] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
