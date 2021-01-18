db9e97e7a8797edc4512a2058a13cc9e66414721
Greg Fletcher <gregfletch@gmail.com>
2021-01-18 15:45:04 -0500

    Prevent users that are already authenticated from being able to access the login or register pages without logging out first (#18)

---
+++ src/app/guards/user.guard.spec.ts
@@ -0,0 +1,81 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
+import { TestBed } from '@angular/core/testing';
+import { ActivatedRouteSnapshot, Router, RouterStateSnapshot } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+
+import { UserGuard } from 'src/app/guards/user.guard';
+import { AuthService } from 'src/app/services/auth.service';
+
+describe('UserGuard', () => {
+  let guard: UserGuard;
+
+  beforeEach(() => {
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule, RouterTestingModule],
+      providers: [AuthService]
+    });
+    guard = TestBed.inject(UserGuard);
+  });
+
+  it('should be created', () => {
+    expect(guard).toBeTruthy();
+  });
+
+  describe('canActivate', () => {
+    let authService: AuthService;
+    let router: Router;
+
+    beforeEach(() => {
+      authService = TestBed.inject(AuthService);
+      router = TestBed.inject(Router);
+
+      spyOn(router, 'navigate');
+    });
+
+    it('returns true if the user is not authenticated and their auth token is not expired', () => {
+      spyOn(authService, 'isAuthenticated').and.returnValue(false);
+      spyOn(authService, 'isTokenExpired').and.returnValue(false);
+
+      expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: 'testUrl' })).toBeTruthy();
+    });
+
+    it('returns true if the user is authenticated and their auth token is expired', () => {
+      spyOn(authService, 'isAuthenticated').and.returnValue(true);
+      spyOn(authService, 'isTokenExpired').and.returnValue(true);
+
+      expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: 'testUrl' })).toBeTruthy();
+    });
+
+    it('returns true if the user is not authenticated and their auth token is expired', () => {
+      spyOn(authService, 'isAuthenticated').and.returnValue(false);
+      spyOn(authService, 'isTokenExpired').and.returnValue(true);
+
+      expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: 'testUrl' })).toBeTruthy();
+    });
+
+    it('does not call router#navigate if the route can activate', () => {
+      spyOn(authService, 'isAuthenticated').and.returnValue(false);
+      spyOn(authService, 'isTokenExpired').and.returnValue(false);
+
+      guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: 'testUrl' });
+
+      expect(router.navigate).not.toHaveBeenCalled();
+    });
+
+    it('returns false if the user is authenticated and their auth token is not expired', () => {
+      spyOn(authService, 'isAuthenticated').and.returnValue(true);
+      spyOn(authService, 'isTokenExpired').and.returnValue(false);
+
+      expect(guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: 'testUrl' })).toBeFalsy();
+    });
+
+    it('calls router#navigate if the route cannot be activated', () => {
+      spyOn(authService, 'isAuthenticated').and.returnValue(true);
+      spyOn(authService, 'isTokenExpired').and.returnValue(false);
+
+      guard.canActivate(new ActivatedRouteSnapshot(), <RouterStateSnapshot>{ url: 'testUrl' });
+
+      expect(router.navigate).toHaveBeenCalledWith(['/']);
+    });
+  });
+});
--- src/app/register/register-routing.module.ts
+++ src/app/register/register-routing.module.ts
@@ -1,13 +1,9 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { UserGuard } from 'src/app/guards/user.guard';
 import { RegisterComponent } from 'src/app/register/register.component';
 
-const routes: Routes = [
-  {
-    path: '',
-    component: RegisterComponent
-  }
-];
+const routes: Routes = [{ path: '', component: RegisterComponent, canActivate: [UserGuard] }];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
